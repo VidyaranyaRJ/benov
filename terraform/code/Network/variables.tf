12ed4af06e5b288fa43ed47873dffd28e838ff97
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-13 19:21:42 -0600

    ben-1-nw-2

--- terraform/code/Network/variables.tf
+++ terraform/code/Network/variables.tf
@@ -10,33 +10,7 @@ variable "instance_type" {
   type        = string
 }
 
-# variable "AWS_ACCESS_KEY_ID" {
-#   type        = string
-# }
 
-# variable "AWS_SECRET_ACCESS_KEY" {
-#   type        = string
-# }
-
-
-
-# variable "sg_name" {
-#   type        = string
-# }
-
-variable "environment" {
-  description = "Environment to deploy to (qa or prod)"
-  type        = string
-}
-# variable "ec2_name" {
-#   type        = string
-#   description = "Name tag for the EC2 instance"
-# }
-
-
-variable "alb_name" {
-  type = string
-}
 
 
 
