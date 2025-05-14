72a27aa34da75e24599df14afbd99cc64c7bd80a
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-13 19:01:35 -0600

    ben-1-

--- terraform/code/EFS/main.tf
+++ terraform/code/EFS/main.tf
@@ -8,13 +8,13 @@ terraform {
 }
 
 locals {
-  creation_token_efs1 = "GitHub - code"
+  creation_token_efs1 = "App -GitHub - code"
   tag_name_efs1 = "App Data"
 
-  creation_token_efs2 = "GitHub - code"
+  creation_token_efs2 = "ORG GitHub - code"
   tag_name_efs2 = "ORG Data"
 
-  creation_token_efs3 = "GitHub - code"
+  creation_token_efs3 = "User GitHub - code"
   tag_name_efs3 = "User Data"
 }
 
