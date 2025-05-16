eed3d5382985110822b359fa77c069f0e178db8e
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-15 20:28:17 -0600

    ben-1-ec2-38-s3-2

---
+++ Nodejs/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! - VJ');
+});
+
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
