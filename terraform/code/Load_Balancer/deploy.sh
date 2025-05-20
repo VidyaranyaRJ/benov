3315035340d62652b67f3d10102a08bb25d14d7e
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-19 21:38:03 -0600

    ben-6-ec2-s3-14

--- terraform/code/Load_Balancer/deploy.sh
+++ terraform/code/Load_Balancer/deploy.sh
@@ -18,4 +18,9 @@ terraform -chdir="$TERRAFORM_DIR" init \
 
 # === Plan and apply ===
 terraform -chdir="$TERRAFORM_DIR" plan -out=tfplan
-terraform -chdir="$TERRAFORM_DIR" apply -auto-approve tfplan
+
+# terraform -chdir="$TERRAFORM_DIR" apply -auto-approve tfplan
+
+
+# === Destroy resources ===
+terraform -chdir="$TERRAFORM_DIR" destroy -auto-approve
