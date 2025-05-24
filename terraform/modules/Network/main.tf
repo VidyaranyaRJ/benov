8b4eaa1415ddf117e53bfb7388594b3e4a91a19b
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-24 15:31:22 -0600

    ben-8-ec2-nw-25

--- terraform/modules/Network/main.tf
+++ terraform/modules/Network/main.tf
@@ -16,26 +16,38 @@ locals {
 
 }
 
-module "benevolate_vpc" {
-  source                    = "../../resources/Network/benevolate_application/vpc"
 
-  #### VPC ####
+
+##### VPC #####
+
+module "benevolate_vpc" {
+
+  source                    = "../../resources/Network/benevolate_application/vpc"
   vpc_tags                  = local.tag_name
   vpc_cidr_block            = local.vpc_cidr_block
   vpc_enable_dns_support    = true
-  vpc_enable_dns_hostnames  = true
+  vpc_enable_dns_hostnames  = true 
 
-  # #### Subnets ####
-  # subnets = {
-  #   Benevolate-subnet-load-balancer-1  = { subnet_cidr = "10.0.1.0/24", subnet_availability_zone = "us-east-2a", subnet_public = true }
-  #   Benevolate-subnet-load-balancer-2  = { subnet_cidr = "10.0.2.0/24", subnet_availability_zone = "us-east-2b", subnet_public = true }
-  #   Benevolate-subnet-application-1    = { subnet_cidr = "10.0.3.0/24", subnet_availability_zone = "us-east-2a", subnet_public = false }
-  # }
-  
-  #### NAT Gateway ####
-  # public_subnet_id_nat_gateway = module.network.subnet_ids["Benevolate-subnet-load-balancer-1"]
-  # tag_name_nat_gateway = local.tag_name
-
-  # sg_name = local.sg_name
-  
 }
+
+
+# ##### SUBNET #####
+
+# module "benevolate_subnet" {
+#   source                    = "../../resources/Network/benevolate_application/subnet"
+
+#   #### Subnets ####
+#   vpc_id = 
+#   subnets = {
+#     Benevolate-subnet-load-balancer-1  = { subnet_cidr = "10.0.1.0/24", subnet_availability_zone = "us-east-2a", subnet_public = true }
+#     Benevolate-subnet-load-balancer-2  = { subnet_cidr = "10.0.2.0/24", subnet_availability_zone = "us-east-2b", subnet_public = true }
+#     Benevolate-subnet-application-1    = { subnet_cidr = "10.0.3.0/24", subnet_availability_zone = "us-east-2a", subnet_public = false }
+#   }
+  
+#   #### NAT Gateway ####
+#   # public_subnet_id_nat_gateway = module.network.subnet_ids["Benevolate-subnet-load-balancer-1"]
+#   # tag_name_nat_gateway = local.tag_name
+
+#   # sg_name = local.sg_name
+  
+# }
