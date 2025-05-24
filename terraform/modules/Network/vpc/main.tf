07af2ac0376b292f1eeb52da3c407789fd4de088
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-24 15:42:46 -0600

    ben-8-ec2-nw-29

--- terraform/modules/Network/vpc/main.tf
+++ terraform/modules/Network/vpc/main.tf
@@ -22,7 +22,7 @@ locals {
 
 module "benevolate_vpc" {
 
-  source                    = "../../resources/Network/benevolate_application/vpc"
+  source                    = "../../../resources/Network/benevolate_application/vpc"
   vpc_tags                  = local.tag_name
   vpc_cidr_block            = local.vpc_cidr_block
   vpc_enable_dns_support    = true
