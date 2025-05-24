5090b2d707f2f9c09935cabee8397de1f1408275
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-24 15:55:15 -0600

    ben-8-ec2-nw-30

--- terraform/modules/Network/vpc/variables.tf
+++ terraform/modules/Network/vpc/variables.tf
@@ -2,13 +2,13 @@ variable "region" {
   type        = string
 }
 
-variable "ami" {
-  type        = string
-}
+# variable "ami" {
+#   type        = string
+# }
 
-variable "instance_type" {
-  type        = string
-}
+# variable "instance_type" {
+#   type        = string
+# }
 
 
 
