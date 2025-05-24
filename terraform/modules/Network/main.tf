012b7134c3677434d6e32a7f32f91b831f41f228
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-24 07:46:59 -0600

    ben-8-ec2-s3-1

--- terraform/modules/Network/main.tf
+++ terraform/modules/Network/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "my_vpc" { #benevolate_vpc
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
@@ -18,6 +18,7 @@ resource "aws_subnet" "my_subnet" {
   }
   depends_on = [  aws_vpc.my_vpc ]
 }
+
 
 
 ###########
