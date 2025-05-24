8b9bdcd590e3111f6891e78678e9f0d0a032410b
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-24 14:17:42 -0600

    ben-8-ec2-nw-17

--- terraform/modules/Network/outputs.tf
+++ terraform/modules/Network/outputs.tf
@@ -9,6 +9,11 @@ output "module_subnet_id" {
   value = module.network.subnet_ids
 }
 
+output "module_subnet_id_1" {
+  value = module.network.subnet_ids[0]
+}
+
+
 
 # output "module_security_group_id" {
 #   value = module.network.security_group_id
