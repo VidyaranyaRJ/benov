a6e0f0e4918a72a5967b4bfaf29b7ddbeb5f2f29
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-24 13:32:45 -0600

    ben-8-ec2-nw-8

--- terraform/resources/Network/benevolate_application_vpc/outputs.tf
+++ terraform/resources/Network/benevolate_application_vpc/outputs.tf
@@ -1,15 +1,14 @@
-output "subnet_id" {
-  value = aws_subnet.my_subnet.id
-}
-
-output "subnet_id_2" {
-  value = aws_subnet.my_subnet_2.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.ecs_security_group.id
-}
-
 output "vpc_id" {
-  value = aws_vpc.my_vpc.id
+  value = aws_vpc.benevolate_vpc.id
 }
+
+
+output "subnet_id" {
+  value = aws_subnet.benevolate_public_subnet.id
+}
+
+
+# output "security_group_id" {
+#   value = aws_security_group.ecs_security_group.id
+# }
+
