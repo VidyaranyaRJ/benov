7a7a14912d2ff2a346a42062d714f68b2f1762f8
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-27 16:34:21 -0600

    ben-11-ec2-nw-18

--- terraform/modules/EC2/outputs.tf
+++ terraform/modules/EC2/outputs.tf
@@ -3,20 +3,20 @@ output "module_instance_1_id" {
 }
 
 
-# output "module_instance_2_id" {
-#   value = module.instance_2.instance_id
-# }
+output "module_instance_2_id" {
+  value = module.instance_2.instance_id
+}
 
 
-# output "module_instance_3" {
-#   value = module.Instance_3.instance_id
+output "module_instance_3" {
+  value = module.instance_3.instance_id
+}
+
+output "module_instance_4" {
+  value = module.instance_4.instance_id
+}
 
 
-# output "module_instance_4" {
-#   value = module.Instance_4.instance_id
-# }
-
-
-# output "module_instance_5" {
-#   value = module.Instance_5.instance_id
-# }
+output "module_instance_5" {
+  value = module.instance_5.instance_id
+}
