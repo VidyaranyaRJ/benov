dd6c5c66f71fb89e0fa7ba402f861a00d9c1d376
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-27 13:47:03 -0600

    node-js-6

--- providers.tf
+++ providers.tf
@@ -1,7 +1,8 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
+  backend "s3" {
+    bucket         = "vj-test-benvolate"
+    key            = "build/terraform.tfstate"  # change per module (e.g., efs/, ec2/)
+    region         = "us-east-2"
+    encrypt        = true     
   }
 }
