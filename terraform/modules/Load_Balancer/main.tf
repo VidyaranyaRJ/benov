9c138932f3e946abf5bdad1f92505b9cde081cba
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-27 21:41:24 -0600

    ben-11-ec2-nw-24

--- terraform/modules/Load_Balancer/main.tf
+++ terraform/modules/Load_Balancer/main.tf
@@ -76,10 +76,10 @@ module "load_balancer_1" {
   security_group_id  = data.terraform_remote_state.security_group.outputs.module_benevolate_security_group_id
   ec2_instance_ids = [
     data.terraform_remote_state.ec2.outputs.module_instance_1_id,
-    data.terraform_remote_state.ec2.outputs.module_instance_2_id,
-    data.terraform_remote_state.ec2.outputs.module_instance_3,
-    data.terraform_remote_state.ec2.outputs.module_instance_4,
-    data.terraform_remote_state.ec2.outputs.module_instance_5
+    # data.terraform_remote_state.ec2.outputs.module_instance_2_id,
+    # data.terraform_remote_state.ec2.outputs.module_instance_3,
+    # data.terraform_remote_state.ec2.outputs.module_instance_4,
+    # data.terraform_remote_state.ec2.outputs.module_instance_5
   ]
   target_group_name = local.target_group_name
   subnet_ids = [
