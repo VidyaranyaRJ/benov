6af28eff49dc5f46d1389a30d66bf9c901b8d743
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-06-15 21:48:26 -0600

    ben-ec2

--- scripts/node-deploy.sh
+++ scripts/node-deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# Fetch EC2 instance ID via IMDSv2
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600" -s)
+
+INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" \
+  -s http://169.254.169.254/latest/meta-data/instance-id)
+
+echo "Running on EC2 instance: $INSTANCE_ID"
+
+
 # Configuration
 S3_BUCKET="vj-test-benvolate"
 S3_KEY="nodejs/nodejs-app.zip"
