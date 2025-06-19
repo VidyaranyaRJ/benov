69dd8252f283e282dba72f73e45f1df4d8112a5b
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-06-19 11:58:49 -0600

    latency-10

--- providers.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "vj-test-benvolate"
-    key            = "build/terraform.tfstate"  # change per module (e.g., efs/, ec2/)
-    region         = "us-east-2"
-    encrypt        = true     
-  }
-}
