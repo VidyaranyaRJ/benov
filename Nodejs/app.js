1190f15b2ae4829a0b687d6700d75ccda776a1ec
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-06-27 20:03:07 -0600

    added /health route

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -77,6 +77,11 @@ app.get('/users', async (req, res) => {
   }
 });
 
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
