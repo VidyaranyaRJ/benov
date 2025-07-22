4104f68e78f0cdcf47f5754530b36f8a01ac2056
Vidyaranya R Javalagi <vrjavalagi779@gmail.com>
2025-07-22 11:34:40 -0600

    modified reset-restart file added hostname

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -4,9 +4,6 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const os = require('os');
 
-const { execSync } = require("child_process");
-
-
 const { faker } = require('@faker-js/faker');
 const { promiseDB } = require('./js/db');
 const logger = require('./js/logger');
@@ -46,8 +43,7 @@ app.get('/insertRandomUser', async (req, res) => {
     const phone = faker.phone.number();
 
     const ip_address = getServerIpAddress();
-    // const host_name = os.hostname();
-    const host_name = execSync("hostname").toString().trim();
+    const host_name = os.hostname();
 
     const [result] = await promiseDB.execute(
       'INSERT INTO stress_test_users (name, email, user_type, phone, ip_address, host_name, testcaseid, misc, ec2instances, acu) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
@@ -126,8 +122,8 @@ app.get('/health', (req, res) => {
 app.get('/', (req, res) => {
   const environment = process.env.ENVIRONMENT || 'EC2 (Default)';
   const ip_address = getServerIpAddress();
-  // const host_name = os.hostname();
-  const host_name = execSync("hostname").toString().trim();
+  const host_name = os.hostname();
+
 
   const current_time = new Date().toLocaleString();
   const memory_used = ((os.totalmem() - os.freemem()) / 1024 / 1024).toFixed(2); // in MB
