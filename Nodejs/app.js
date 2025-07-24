58374f2bd73eb9f03c418a504c03bad2fb48fab9
Vidyaranya R Javalagi <vrjavalagi779@gmail.com>
2025-07-24 16:14:33 -0600

    uat

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -10,6 +10,8 @@ const logger = require('./js/logger');
 
 const app = express();
 
+
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
